ddf757fbc9152d435a87554341c4b424a4a7df6e
felixyunianto <felixyunianto@gmail.com>
2021-08-31 11:08:19 +0700

    5. Auth Controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common';
+import { Request } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserDto } from '../users/dto/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService : AuthService){}
+
+    @UseGuards(AuthGuard('local'))
+    @Post('login')
+    async login(@Request() req){
+        return await this.authService.login(req.user);
+    }
+
+    @Post('signup')
+    async signUp(@Body() user: UserDto){
+        return await this.authService.create(user)
+    }
+}
