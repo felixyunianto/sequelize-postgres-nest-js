d131d88c3890979d1d8f0d79c906365311eef5b6
felixyunianto <felixyunianto@gmail.com>
2021-09-03 09:38:09 +0700

    6. Validation on progress

---
+++ src/core/pipes/validate.pipe.ts
@@ -0,0 +1,25 @@
+import {
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+  ValidationPipe,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+
+@Injectable()
+export class ValidateInputPipe extends ValidationPipe {
+  public async transform(value, metadata: ArgumentMetadata) {
+    try {
+      return await super.transform(value, metadata);
+    } catch (e) {
+      if (e instanceof BadRequestException) {
+        //   console.log(e.message)
+        throw new UnprocessableEntityException(this.handleError(e));
+      }
+    }
+  }
+
+  private handleError(errors) {
+    return errors.response.message.map((error) => error)
+  }
+}
